05eb20fc796e95b3bd0c7d72be39a9dfd6656406
William McDonald <ginoskotheon@yahoo.com>
2016-09-28 11:49:42 -0500

    More
    Stuff!

--- server.js
+++ server.js
@@ -13,13 +13,32 @@ app.get('/', function(req, res){
 
 app.get('/new/:url', function(req, res){
     var url = req.url.slice(5);
-    var result = {
-        "URL": url
+    var result = {};
+    if(validateURL(url)){
+        result = {
+            "URL": url,
+            "Hobbit URL": process.env.APP_URL + linkGen()
+        };
+    }
+    else {
+        result = {
+        "error": "No hobbits here."
+        };
     }
     res.send(result);
 });
 
+function linkGen() {
+    var num = Math.floor(100000 + Math.random() * 900000);
+    return num.toString().substring(0, 4);
+}
 
+function validateURL(url) {
+    // Checks to see if it is an actual url
+    // Regex from https://gist.github.com/dperini/729294
+    var regex = /^(?:(?:https?|ftp):\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,}))\.?)(?::\d{2,5})?(?:[/?#]\S*)?$/i;
+    return regex.test(url);
+  }
 
 
 
