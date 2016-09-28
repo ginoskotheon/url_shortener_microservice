a39fec87787efa43315a474613666831dc138dbe
William McDonald <ginoskotheon@yahoo.com>
2016-09-28 14:05:22 -0500

    some change

--- server.js
+++ server.js
@@ -17,9 +17,9 @@ app.get('/', function(req, res){
 
 
 
- app.get('/:url', function (req, res){
+ app.get('/:url*', function (req, res){
     var home = "https://sleepy-savannah-67922.herokuapp.com/"
-    var url = req.url.slice(4);
+    var url = req.url.slice(3);
     var result = {};
     if(validateURL(url)){
         result = {
